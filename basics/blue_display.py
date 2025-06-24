7b11e5830e885bccf14de64cdcf1bed3bd1653ee
Lucky-Lakshay <luckatmyside99@gmail.com>
2025-06-25 00:32:26 +0530

    init pygame, set display mode, fill color

---
+++ basics/blue_display.py
@@ -0,0 +1,18 @@
+import pygame
+
+# display
+pygame.init()
+window_width, window_height = 1280, 720
+display_surface = pygame.display.set_mode((window_width, window_height))
+running = True
+
+while running:
+    # event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    # draw the game
+    display_surface.fill("blue")
+    pygame.display.update()
+
+pygame.quit()
