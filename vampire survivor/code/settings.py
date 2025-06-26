8ab2a2945090c5b30feea20232bb69cf84312750
Lucky-Lakshay <luckatmyside99@gmail.com>
2025-06-26 23:58:27 +0530

    feat: camera

--- vampire survivor/code/settings.py
+++ vampire survivor/code/settings.py
@@ -1,6 +1,8 @@
 import pygame 
 from os.path import join 
 from os import walk
+from random import randint
+from pytmx.util_pygame import load_pygame
 
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280,720 
 TILE_SIZE = 64
--- vampire survivor/code/vs_main.py
+++ vampire survivor/code/vs_main.py
@@ -1,21 +1,55 @@
 from settings import *
+from player import Player
+from sprites import *
+from groups import allsprites
 
 class Game:
     def __init__(self):
+        #setup
         pygame.init()
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption("Vampire Survivor")
         self.clock = pygame.time.Clock()
         self.running = True
         self.game_active = True
+
+        #groups
+        self.all_sprites = allsprites()
+        self.collision_sprites = pygame.sprite.Group()
+
+        self.setup()
+
+    def setup(self):
+        map = load_pygame(join("vampire survivor", "data", "maps", "world.tmx"))
+        for x, y, image in map.get_layer_by_name("Ground").tiles():
+            Sprite((x* TILE_SIZE, y* TILE_SIZE), image, self.all_sprites)
+        for obj in map.get_layer_by_name("Objects"):
+            Collisionsprite((obj.x, obj.y), obj.image, (self.all_sprites, self.collision_sprites))
+        for obj in map.get_layer_by_name("Collisions"):
+            Collisionsprite((obj.x, obj.y), pygame.Surface((obj.width, obj.height)), self.collision_sprites)
+        for obj in map.get_layer_by_name("Entities"):
+            if obj.name == "Player":
+                self.player = Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
+
     def run(self):
         while self.running:
             dt = self.clock.tick() / 1000
+
+            #event loop
             for event in pygame.event.get():
                 if event.type == pygame.QUIT: 
                     self.running = False
-            pygame.display.update()        
+            
+            #update
+            self.all_sprites.update(dt)
+
+            #draw 
+            self.display_surface.fill("black")
+            self.all_sprites.draw(self.player.rect.center)
+            pygame.display.update()
+
         pygame.quit()
 
 if __name__ == "__main__":
-    Game().run()
+    game = Game()
+    game.run()
