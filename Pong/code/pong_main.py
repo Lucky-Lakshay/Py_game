da01704ee7a751ba12494fd36f2ca3d33adb0d3c
Lucky-Lakshay <luckatmyside99@gmail.com>
2025-07-07 17:56:05 +0530

    feat: opponent, ball reset | style: shadows

--- Pong/code/pong_main.py
+++ Pong/code/pong_main.py
@@ -1,5 +1,7 @@
 from settings import * 
 from sprites import *
+import json
+from groups import Allsprites
 
 class Game:
     def __init__(self):
@@ -10,10 +12,36 @@ class Game:
         self.running = True
 
         #sprites
-        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites = Allsprites()
         self.paddle_sprites = pygame.sprite.Group()
         self.player = Player((self.all_sprites, self.paddle_sprites))
-        self.ball = Ball(self.all_sprites, self.paddle_sprites)
+        self.ball = Ball(self.all_sprites, self.paddle_sprites, self.update_score)
+        Opponent((self.all_sprites,self.paddle_sprites), self.ball)
+
+        #score
+        try:
+            with open(join("pong", "data", "score.txt")) as score_file:
+                self.score = json.load(score_file)
+        except:        
+            self.score = {"player": 0, "opponent": 0}
+        self.font = pygame.font.Font(None, 160)
+
+    def display_score(self):
+        #Player
+        player_surf = self.font.render(str(self.score["player"]), True, COLORS["bgdetail"])
+        player_rect = player_surf.get_frect(center = (WINDOW_WIDTH/2 + 100, WINDOW_HEIGHT/2))
+        self.display_surface.blit(player_surf, player_rect)
+
+        #Opponent
+        opponent_surf = self.font.render(str(self.score["opponent"]), True, COLORS["bgdetail"])
+        opponent_rect = opponent_surf.get_frect(center = (WINDOW_WIDTH/2 - 100, WINDOW_HEIGHT/2))
+        self.display_surface.blit(opponent_surf, opponent_rect)
+
+        #line separator
+        pygame.draw.line(self.display_surface, COLORS["bgdetail"], (WINDOW_WIDTH/2, 0), (WINDOW_WIDTH/2, WINDOW_HEIGHT), 6)
+
+    def update_score(self, side):
+        self.score["player" if side == "player" else "opponent"] += 1
 
     def run(self):
         while self.running:
@@ -23,13 +51,16 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT: 
                     self.running = False
+                    with open(join("pong", "data", "score.txt"), "w") as score_file:
+                        json.dump(self.score, score_file)
 
             #update
             self.all_sprites.update(dt)        
 
             #draw 
             self.display_surface.fill(COLORS["bg"])
-            self.all_sprites.draw(self.display_surface)
+            self.display_score()
+            self.all_sprites.draw()
             pygame.display.update()
 
         pygame.quit()
--- Pong/code/settings.py
+++ Pong/code/settings.py
@@ -4,11 +4,12 @@ from os.path import join
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720 
 SIZE = {'paddle': (40,100), 'ball': (30,30)}
 POS = {'player': (WINDOW_WIDTH - 30, WINDOW_HEIGHT / 2), 'opponent': (30, WINDOW_HEIGHT / 2)}
-SPEED = {'player': 500, 'opponent': 250, 'ball': 450}
+SPEED = {'player': 700, 'opponent': 335, 'ball': 450}
 COLORS = {
     'paddle': '#ee322c',
     'paddle shadow': '#b12521',
     'ball': '#ee622c',
     'ball shadow': '#c14f24',
-    'bg': '#002633'
+    'bg': '#002633',
+    'bgdetail': '#004a63'
 }
